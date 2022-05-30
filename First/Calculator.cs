22cfce40d16e5181b9ccb98404dc9a1f891407a9
Martin <martinjonsson01@gmail.com>
2022-05-30 13:44:17 +0200

    Add calculator

---
+++ First/Calculator.cs
@@ -0,0 +1,12 @@
+﻿namespace First;
+
+public class Calculator
+{
+    public int FirstNumber  { get; set; }
+    public int SecondNumber { get; set; }
+
+    public int Add()
+    {
+        return FirstNumber + SecondNumber;
+    }
+}
