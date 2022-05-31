74e1ce991250fdd6130d0cf54a3c269d30f6f00d
Martin <martinjonsson01@gmail.com>
2022-05-31 15:38:47 +0200

    Add unit test example projects

---
+++ tests/First.UnitTests/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿global using System;
+global using System.Collections.Generic;
+
+global using Bogus;
+
+global using First;
+
+global using FluentAssertions;
+
+global using Moq;
+
+global using Xunit;
---
+++ tests/Second.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Second.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test_That_Always_Passes_When_True()
+    {
+        true.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test_That_Always_Passes_When_False()
+    {
+        false.Should().BeFalse();
+    }
+}
