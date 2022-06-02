95dc1092b6a7f0b1c85406f0b9423122925557c3
Martin <martinjonsson01@gmail.com>
2022-06-02 11:15:09 +0200

    Add missing example tests for calculator

--- tests/First.UnitTests/UnitTest1.cs
+++ tests/First.UnitTests/UnitTest1.cs
@@ -2,6 +2,8 @@ namespace First.UnitTests;
 
 public class UnitTest1
 {
+    private readonly Calculator _calculator = new ();
+
     [Fact]
     public void Test_That_Always_Passes_When_True()
     {
@@ -12,5 +14,35 @@ public class UnitTest1
     public void Test_That_Always_Passes_When_False()
     {
         false.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Adding_Zero_Does_Not_Change_Number()
+    {
+        // Arrange
+        const int number = 12;
+        _calculator.FirstNumber = number;
+        _calculator.SecondNumber = 0;
+
+        // Act
+        int result = _calculator.Add();
+
+        // Assert
+        result.Should().Be(number);
+    }
+
+    [Fact]
+    public void Subtracting_Zero_Does_Not_Change_Number()
+    {
+        // Arrange
+        const int number = 12;
+        _calculator.FirstNumber = number;
+        _calculator.SecondNumber = 0;
+
+        // Act
+        int result = _calculator.Subtract();
+
+        // Assert
+        result.Should().Be(number);
     }
 }
